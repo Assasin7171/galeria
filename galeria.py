8f3c28d75b78990158f942ad3488f3faa4bffacf
Assasin7171 <kbojanecki@gmail.com>
2022-12-15 16:33:50 +0100

    Wprowadzono nieznaczne zmiany kodu.

--- galeria.py
+++ galeria.py
@@ -7,22 +7,29 @@ import os
 https://tkdocs.com/tutorial/widgets.html
 https://tkdocs.com/tutorial/index.html
 """
+imgList = list()
 
+folder_path = '../galeria/img'
+for filename in os.listdir(folder_path):
+    if filename.endswith(".JPG"):
+        imgList.append(filename)
 
-def openAllImg():
-    for filename in os.listdir("files"):
-        return
 root = tk.Tk()
 root.title("Galeria")
 #root.geometry("800x600")
+def do_Przodu():
+    
+    return
+
 
 frame = ttk.Frame(root, borderwidth=5, relief="groove").grid(row=0, column=0)
 
 
-img1 = ImageTk.PhotoImage(Image.open("img/zdj1.jpg").resize([700, 500]))
+img1 = ImageTk.PhotoImage(Image.open("img/8.JPG").resize([700, 500]))
+
 labelImage = tk.Label(frame, image=img1).grid(row=1,column=0)
 
-btnRight = tk.Button(root, text="Do przodu").grid(sticky="E",row=2)
+btnRight = tk.Button(root, text="Do przodu",command=do_Przodu).grid(sticky="E",row=2)
 btnLeft = tk.Button(root, text="Do tyłu").grid(sticky="W",row=2)
 
 root.mainloop()
