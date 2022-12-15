06caf66cf8ff54ec2bace9f200fbd922ceaca057
Assasin7171 <kbojanecki@gmail.com>
2022-12-15 15:11:31 +0100

    Dodano przycisk do przodu i do tyłu.

--- galeria.py
+++ galeria.py
@@ -1,24 +1,28 @@
 import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
-import os as os
+import os
 
 """
 https://tkdocs.com/tutorial/widgets.html
 https://tkdocs.com/tutorial/index.html
 """
+
+
 def openAllImg():
     for filename in os.listdir("files"):
         return
 root = tk.Tk()
 root.title("Galeria")
-root.geometry("800x600")
+#root.geometry("800x600")
 
 frame = ttk.Frame(root, borderwidth=5, relief="groove").grid(row=0, column=0)
 
 
 img1 = ImageTk.PhotoImage(Image.open("img/zdj1.jpg").resize([700, 500]))
-labelImage = tk.Label(frame, image=img1).grid(row=3, column=0)
+labelImage = tk.Label(frame, image=img1).grid(row=1,column=0)
 
+btnRight = tk.Button(root, text="Do przodu").grid(sticky="E",row=2)
+btnLeft = tk.Button(root, text="Do tyłu").grid(sticky="W",row=2)
 
 root.mainloop()
